8a248e690519ad90a1cbc8320620f364b1b4c27a
moddthing <155221709+moddthing@users.noreply.github.com>
2024-01-13 11:34:39 +0400

    Update snipe.lua

--- snipe.lua
+++ snipe.lua
@@ -134,9 +134,12 @@ end
 
 local function tryPurchase(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
     signal = game:GetService("RunService").Heartbeat:Connect(function()
-	if buytimestamp < workspace:GetServerTimeNow() - Players.LocalPlayer:GetNetworkPing() then
+	if buytimestamp < workspace:GetServerTimeNow() then
 	    signal:Disconnect()
 	    signal = nil
+        end
+    end)
+    repeat task.wait() until signal == nil
         end
     end)
     repeat task.wait() until signal == nil
