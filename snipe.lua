6c71f6c2694bdabd07855aa2dcbd68471ad17b5e
moddthing <155221709+moddthing@users.noreply.github.com>
2024-01-14 21:04:31 +0400

    Update snipe.lua

--- snipe.lua
+++ snipe.lua
@@ -1,4 +1,81 @@
 --start of script (credit to root, Edmond,  ethereum, chocolog)
+
+local startTime = 0
+local isPlaying = false
+local serverHopTime = os.clock() + math.random(720, 920)
+local hopWarningTime = 60
+local isHopScheduled = false
+
+local hopTimer = 0
+local hopMessage = ""
+
+RegisterCommand("playtime", function()
+    isPlaying = not isPlaying
+end, false)
+
+RegisterNetEvent('playerConnecting')
+AddEventHandler('playerConnecting', function()
+    startTime = os.time()
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        TriggerEvent('displayPlaytime')
+        updateHopTimer()
+        TriggerEvent('displayHopTimer')
+    end
+end)
+
+RegisterNetEvent('displayPlaytime')
+AddEventHandler('displayPlaytime', function()
+    if isPlaying then
+        local currentTime = os.time()
+        local playtime = currentTime - startTime
+        local hours = math.floor(playtime / 3600)
+        local minutes = math.floor((playtime % 3600) / 60)
+        local seconds = playtime % 60
+
+        SetTextFont(4)
+        SetTextProportional(0)
+        SetTextScale(2.0, 2.0)
+        SetTextColour(255, 255, 0, 255)
+        SetTextEntry('STRING')
+        AddTextComponentString('^2Your playtime:^7 ' .. hours .. ' hours, ' .. minutes .. ' minutes, and ' .. seconds .. ' seconds.')
+        DrawText(0.5, 0.5)
+    end
+end)
+
+RegisterNetEvent('displayHopTimer')
+AddEventHandler('displayHopTimer', function()
+    if isHopScheduled then
+        SetTextFont(4)
+        SetTextProportional(0)
+        SetTextScale(2.0, 2.0)
+        SetTextColour(255, 0, 0, 255)
+        SetTextEntry('STRING')
+        AddTextComponentString('^1Server hop in ' .. hopTimer .. 's^7')
+        DrawText(0.5, 0.6)
+    end
+end)
+
+function updateHopTimer()
+    local currentTime = os.clock()
+
+    if currentTime >= serverHopTime - hopWarningTime and not isHopScheduled then
+        displayHopNotification()
+        isHopScheduled = true
+    end
+
+    if currentTime >= serverHopTime then
+        jumpToServer()
+        serverHopTime = currentTime + math.random(720, 920)
+        isHopScheduled = false
+    else
+        hopTimer = math.ceil(serverHopTime - currentTime)
+    end
+end
+
 local osclock = os.clock()
 if not game:IsLoaded() then
     game.Loaded:Wait()
